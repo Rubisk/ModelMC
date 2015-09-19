6361316a2e7e3909f7fe284586b17a1fb9619342
Rubisk <rubiskmc@gmail.com>
2015-09-20 00:24:43 +0200

    Added memory leak detector. Stolen from github.com/ymx/gtest_mem Somehow, I didn't have one leak. yay. Detector probably broke.

--- util/runner.cpp
+++ util/runner.cpp
@@ -1,7 +1,37 @@
+#include <iostream>
+#include <crtdbg.h>
 #include <gtest/gtest.h>
 
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
+using namespace std;
+
+namespace testing {
+
+class MemoryLeakDetector : public EmptyTestEventListener {
+public:
+  virtual void OnTestStart(const TestInfo&) {
+    _CrtMemCheckpoint(&memState_);
+  }
+
+  virtual void OnTestEnd(const TestInfo& test_info) {
+    if (test_info.result()->Passed()) {
+      _CrtMemState stateNow, stateDiff;
+      _CrtMemCheckpoint(&stateNow);
+      int diffResult = _CrtMemDifference(&stateDiff, &memState_, &stateNow);
+      if (diffResult) {
+        FAIL() << "Memory leak of " << stateDiff.lSizes[1] << " byte(s) detected.";
+      }
+    }
+  }
+
+private:
+  _CrtMemState memState_;
+};
+}
+
+GTEST_API_ int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  testing::UnitTest::GetInstance()->listeners().Append(new testing::MemoryLeakDetector());
+
   RUN_ALL_TESTS();
   system("pause");
 }
